a715e3eaab44862724f735b9f9dcf82942fd9222
zhengguang <zg.zhu@daocloud.io>
2024-06-15 13:37:18 +0800

    support 1.29 of k8s

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+KUBECONFIG=/root/.kube/config
+kubectl apply -f deployment/lxcfs-daemonset.yaml 
 
-./deployment/webhook-create-signed-cert.sh
+kubectl delete secret lxcfs-admission-webhook-certs
+kube-webhook-certgen --kubeconfig $KUBECONFIG create --namespace default --host lxcfs-admission-webhook-svc.default.svc  --secret-name lxcfs-admission-webhook-certs --key-name key.pem --cert-name cert.pem 
 kubectl get secret lxcfs-admission-webhook-certs
 
+kubectl delete -f deployment/deployment.yaml
 kubectl create -f deployment/deployment.yaml
-kubectl create -f deployment/service.yaml
-cat ./deployment/mutatingwebhook.yaml | ./deployment/webhook-patch-ca-bundle.sh > ./deployment/mutatingwebhook-ca-bundle.yaml
-kubectl create -f deployment/mutatingwebhook-ca-bundle.yaml
+kubectl apply -f deployment/service.yaml
+export CA_BUNDLE=$(kubectl get secrets lxcfs-admission-webhook-certs -o jsonpath='{.data.ca}' | tr -d '\n')
 
+export CA_BUNDLE=""
+
+cat ./deployment/mutatingwebhook.yaml | sed -e "s@\${CA_BUNDLE}@${CA_BUNDLE}@g" > ./deployment/mutatingwebhook-ca-bundle.yaml
+kubectl apply -f deployment/mutatingwebhook-ca-bundle.yaml
+#kube-webhook-certgen --kubeconfig /root/.kube/dev.d.run.worker01.config patch --namespace default --secret-name lxcfs-admission-webhook-certs --webhook-name mutating-lxcfs-admission-webhook-cfg
